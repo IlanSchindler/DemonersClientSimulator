d04350208a80918dabfc5e0ae54c8e4fa2fd17c0
Ilan Schindler <schindim@mail.uc.edu>
2021-08-07 10:50:33 -0400

    initial ui and backend files

---
+++ DemonersClientSimulator/Shared/ConnectionModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemonersClientSimulator.Shared {
+  public class LanguagesComboModel {
+    public string LanguageName { get; set; }
+    public int LanguageId { get; set; }
+  }
+
+  public class ProjectTypeComboModel {
+    public string ProjectTypeName { get; set; }
+    public int ProjectTypeId { get; set; }
+  }
+
+  public class DifficultyComboModel {
+    public string DifficultyName { get; set; }
+    public int DifficultyId { get; set; }
+  }
+}
